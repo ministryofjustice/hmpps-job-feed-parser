82c4b59e46db3a69ed65a52cef0ca3c974489f93
Rohan Salunkhe <rohan.salunkhe@digital.justice.gov.uk>
2018-01-09 15:23:58 +0000

    rss feed parser class added

--- lib/wcn_scraper/rss_feed.rb
+++ lib/wcn_scraper/rss_feed.rb
@@ -3,11 +3,23 @@ require 'rss'
 
 module WcnScraper
   class RssFeed
-    ENDPOINT = 'hahah'
+    ENDPOINT = 'https://justicejobs.tal.net/vx/mobile-0/appcentre-1/brand-2/candidate/jobboard/vacancy/3/feed'
 
-    def initialize
-      uri = URI.parse(ENDPOINT)
-      Net::HTTP.get(uri)
+    def get_vacancies_data
+      url= URI.parse(ENDPOINT)
+      rss = Net::HTTP.get(url)
+      rss = get_rss_content rss
+      list = filter_feed_items(rss,/prison.officer/i)
+    end
+
+    def get_rss_content(url)
+      RSS::Parser.parse(url)
+    end
+
+    def filter_feed_items(rss_feed, regexp)
+      rss_feed.items.find_all do |item|
+        !regexp.match(item.title.content).nil?
+      end
     end
   end
 end
