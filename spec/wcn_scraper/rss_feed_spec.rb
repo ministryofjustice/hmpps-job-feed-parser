63b3fa39fdf0b5ff29aaa026f6aef6fb8e0d67c1
Daniel Penny <daniel.penny@digital.justice.gov.uk>
2018-01-10 15:00:06 +0000

    Remove Rubocop offenses
    To comply

--- spec/wcn_scraper/rss_feed_spec.rb
+++ spec/wcn_scraper/rss_feed_spec.rb
@@ -6,11 +6,11 @@ describe WcnScraper::RssFeed do
     expect(described_class::ENDPOINT).not_to be_empty
   end
 
-  it 'should load an rss file' do
+  it 'Loads an rss file' do
     stub_request(:any, described_class::ENDPOINT).
       to_return(body: File.open('./spec/fixtures/feed.xml'), status: 200)
 
-    rss_feed = WcnScraper::RssFeed.new()
+    rss_feed = described_class.new
     vacancies = rss_feed.get_vacancies_data
     expect(vacancies.count).to eq(54)
   end
