3b51f948d70a75d1a89660f3a1c0b9f43b017390
Daniel Penny <daniel.penny@digital.justice.gov.uk>
2018-01-15 10:08:17 +0000

    Fix Rubocop offenses
    to comply

--- lib/wcn_scraper/rss_feed.rb
+++ lib/wcn_scraper/rss_feed.rb
@@ -5,14 +5,14 @@ module WcnScraper
   class RssFeed
     ENDPOINT = 'https://justicejobs.tal.net/vx/mobile-0/appcentre-1/brand-2/candidate/jobboard/vacancy/3/feed'
 
-    def get_vacancies_data
-      url= URI.parse(ENDPOINT)
+    def vacancies_data
+      url = URI.parse(ENDPOINT)
       rss = Net::HTTP.get(url)
-      rss = get_rss_content rss
-      list = filter_feed_items(rss,/prison.officer/i)
+      rss = rss_content rss
+      filter_feed_items(rss, /prison.officer/i)
     end
 
-    def get_rss_content(url)
+    def rss_content(url)
       RSS::Parser.parse(url)
     end
 
