64e0b3258280051e377affc1430abddef6efa443
Daniel Penny <daniel.penny@digital.justice.gov.uk>
2018-03-19 13:42:33 +0000

    Expectation of format to include prison type
    We will be creating two output files
    Keeping the prisons output
    Adding a Youth custody output

--- spec/vacancy_formatter_spec.rb
+++ spec/vacancy_formatter_spec.rb
@@ -4,10 +4,10 @@ describe VacancyFormatter do
   before do
     stub_const('PRISONS',
       [
-        { name: 'HMP/YOI Downview', town: 'Sutton', lat: 51.338463, lng: -0.188044 },
-        { name: 'HMP/YOI Isle of Wight', town: 'Newport', lat: 50.713196, lng: -1.3076464 },
-        { name: 'HMP Littlehey', town: 'Huntingdon', lat: 52.2805913, lng: -0.3122374 },
-        { name: 'HMP Stocken', town: 'Oakham', lat: 52.7469327, lng: -0.5821626999999999 }
+        { name: 'HMP/YOI Downview', town: 'Sutton', lat: 51.338463, lng: -0.188044, type: 'Youth Custody' },
+        { name: 'HMP/YOI Isle of Wight', town: 'Newport', lat: 50.713196, lng: -1.3076464, type: 'Youth Custody' },
+        { name: 'HMP Littlehey', town: 'Huntingdon', lat: 52.2805913, lng: -0.3122374, type: 'Prison' },
+        { name: 'HMP Stocken', town: 'Oakham', lat: 52.7469327, lng: -0.5821626999999999, type: 'Prison' }
       ])
   end
 
@@ -65,7 +65,8 @@ describe VacancyFormatter do
             closing_date: '07/07/2017',
             prison_name: 'HMP/YOI Downview',
             prison_location: { town: 'Sutton', lat: 51.338463, lng: -0.188044 },
-            url: 'https://justicejobs.tal.net/vx/mobile-0/appcentre-1/brand-13/candidate/so/pm/1/pl/3/opp/9908-201706-Prison-Officer-HMP-YOI-Downview/en-GB'
+            url: 'https://justicejobs.tal.net/vx/mobile-0/appcentre-1/brand-13/candidate/so/pm/1/pl/3/opp/9908-201706-Prison-Officer-HMP-YOI-Downview/en-GB',
+            type: 'Prison'
           },
           {
             title: '201711: Prison Officer - HMP Littlehey & HMP Stocken',
@@ -74,7 +75,8 @@ describe VacancyFormatter do
             closing_date: '30/11/2017',
             prison_name: 'HMP Littlehey',
             prison_location: { town: 'Huntingdon', lat: 52.2805913, lng: -0.3122374 },
-            url: 'https://justicejobs.tal.net/vx/mobile-0/appcentre-1/brand-13/candidate/so/pm/1/pl/3/opp/14225-201711-Prison-Officer-HMP-Littlehey-HMP-Stocken/en-GB'
+            url: 'https://justicejobs.tal.net/vx/mobile-0/appcentre-1/brand-13/candidate/so/pm/1/pl/3/opp/14225-201711-Prison-Officer-HMP-Littlehey-HMP-Stocken/en-GB',
+            type: 'Prison'
           },
           {
             title: '201711: Prison Officer - HMP Littlehey & HMP Stocken',
@@ -83,7 +85,8 @@ describe VacancyFormatter do
             closing_date: '30/11/2017',
             prison_name: 'HMP Stocken',
             prison_location: { town: 'Oakham', lat: 52.7469327, lng: -0.5821626999999999 },
-            url: 'https://justicejobs.tal.net/vx/mobile-0/appcentre-1/brand-13/candidate/so/pm/1/pl/3/opp/14225-201711-Prison-Officer-HMP-Littlehey-HMP-Stocken/en-GB'
+            url: 'https://justicejobs.tal.net/vx/mobile-0/appcentre-1/brand-13/candidate/so/pm/1/pl/3/opp/14225-201711-Prison-Officer-HMP-Littlehey-HMP-Stocken/en-GB',
+            type: 'Prison'
           },
           {
             title: '201710: Prison Officer - HMP/YOI Isle of Wight',
@@ -92,7 +95,8 @@ describe VacancyFormatter do
             closing_date: '30/11/2017',
             prison_name: 'HMP/YOI Isle of Wight',
             prison_location: { town: 'Newport', lat: 50.713196, lng: -1.3076464 },
-            url: 'https://justicejobs.tal.net/vx/mobile-0/appcentre-1/brand-13/candidate/so/pm/1/pl/3/opp/13634-201710-Prison-Officer-HMP-YOI-Isle-of-Wight/en-GB'
+            url: 'https://justicejobs.tal.net/vx/mobile-0/appcentre-1/brand-13/candidate/so/pm/1/pl/3/opp/13634-201710-Prison-Officer-HMP-YOI-Isle-of-Wight/en-GB',
+            type: 'Prison'
           }
         ]
       end
@@ -135,7 +139,8 @@ describe VacancyFormatter do
             closing_date: '07/07/2017',
             prison_name: 'HMP/YOI Downview',
             prison_location: { town: 'Sutton', lat: 51.338463, lng: -0.188044 },
-            url: 'https://justicejobs.tal.net/vx/mobile-0/appcentre-1/brand-13/candidate/so/pm/1/pl/3/opp/9908-201706-Prison-Officer-HMP-YOI-Downview/en-GB'
+            url: 'https://justicejobs.tal.net/vx/mobile-0/appcentre-1/brand-13/candidate/so/pm/1/pl/3/opp/9908-201706-Prison-Officer-HMP-YOI-Downview/en-GB',
+            type: 'Prison'
           }
         ]
       end
@@ -176,7 +181,8 @@ describe VacancyFormatter do
             closing_date: '30/11/2017',
             prison_name: 'HMP Littlehey',
             prison_location: { town: 'Huntingdon', lat: 52.2805913, lng: -0.3122374 },
-            url: 'https://justicejobs.tal.net/vx/mobile-0/appcentre-1/brand-13/candidate/so/pm/1/pl/3/opp/14225-201711-Prison-Officer-HMP-Littlehey-HMP-Stocken/en-GB'
+            url: 'https://justicejobs.tal.net/vx/mobile-0/appcentre-1/brand-13/candidate/so/pm/1/pl/3/opp/14225-201711-Prison-Officer-HMP-Littlehey-HMP-Stocken/en-GB',
+            type: 'Prison'
           },
           {
             title: '201711: Prison Officer - HMP Littlehey & HMP Stocken',
@@ -185,7 +191,8 @@ describe VacancyFormatter do
             closing_date: '30/11/2017',
             prison_name: 'HMP Stocken',
             prison_location: { town: 'Oakham', lat: 52.7469327, lng: -0.5821626999999999 },
-            url: 'https://justicejobs.tal.net/vx/mobile-0/appcentre-1/brand-13/candidate/so/pm/1/pl/3/opp/14225-201711-Prison-Officer-HMP-Littlehey-HMP-Stocken/en-GB'
+            url: 'https://justicejobs.tal.net/vx/mobile-0/appcentre-1/brand-13/candidate/so/pm/1/pl/3/opp/14225-201711-Prison-Officer-HMP-Littlehey-HMP-Stocken/en-GB',
+            type: 'Prison'
           }
         ]
       end
